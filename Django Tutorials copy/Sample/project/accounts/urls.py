84c505930bc68d1116a7e724a6c4b1d65ae68076
Dhusnic Infant D M <dhusnicinfant@gmail.com>
2024-07-31 15:59:17 +0530

    tenth commit 31-07-24 registering a new user in travello registration and Login process for django

--- Django Tutorials copy/Sample/project/accounts/urls.py
+++ Django Tutorials copy/Sample/project/accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 from . import models
 
-urlpatterns=[    path("register",views.register,name='register'),
+urlpatterns=[    
+    path("register",views.register,name='register'),
+    path("login",views.login,name="login")
 ]
--- Django Tutorials copy/Sample/project/accounts/views.py
+++ Django Tutorials copy/Sample/project/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 def register(request):
     if request.method=="POST":
         first_name=request.POST['first_name']
@@ -11,18 +13,34 @@ def register(request):
         if password==confirm_password:
             if User.objects.filter(username=user_name).exists():
                 print("UserName Taken")
+                messages.info(request,"User Id is Taken")
             elif User.objects.filter(email=email).exists():
                 print("email Taken")
-            
-        
+                messages.info(request,"Email Id is Taken")
+
             else:
-                user=User.objects.create_user(username=user_name,password=Password1,email=email,first_name=first_name,last_name=last_name)
+                user=User.objects.create_user(username=user_name,password=password,email=email,first_name=first_name,last_name=last_name)
                 user.save()
                 print("user created")
-                return redirect("/travello")
+                return redirect("login")
         else:
+            messages.info(request,"Email Id is Taken")
             print("Password is not matching")
-        return redirect("/travello/accounts/register")
+        return redirect('register')
 
     else:
         return render(request,"register.html")
+    
+def login(request):
+    if request.method=='POST':
+        username=request.POST['user_name']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/travello")
+        else:
+            messages.info(request,'invalid Credentials')
+            return redirect('login')
+    else:
+     return render(request,"login.html")
